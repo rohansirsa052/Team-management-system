e8ac60275a5d485e0fe9f9bb142d9135c9ee882f
Rohan Sirsa <rohansirsa052@gmail.com>
2024-04-03 20:21:55 +0530

    third-commit

--- back_end/app.js
+++ back_end/app.js
@@ -2,17 +2,19 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const cors = require("cors"); 
+require('dotenv').config();
 
-app.use(
-    cors({
-      origin: "http://localhost:3000", 
-    })
-  );
+app.use(cors());
+
 
   mongoose
-  .connect("mongodb://127.0.0.1:27017/user_manager")
+  .connect(process.env.MONGODB_CONNECT_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => console.error("Could not connect to MongoDB", err));
+
 
 
   const userSchema = new mongoose.Schema({
